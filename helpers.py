a6304c071c6c6a4f6a156eb3c1bc9acd96c28ad3
Winston Lam <winston.lam@outlook.com>
2018-01-17 15:19:00 +0000

    helpers.py toegevoegd

---
+++ helpers.py
@@ -0,0 +1,11 @@
+
+def apology()
+"returned een excuus als de user een veld leeg of niet correct invult"
+def login_required()
+"zorgt ervoor dat een user eers moet inloggen alvorens een actie uit te voeren"
+def rate()
+"deze functie zorgt ervoor dat een user foto's kan raten"
+def follow()
+"deze functie zorgt ervoor dat een user mensen kan volgen"
+def unfollow()
+"deze functie zorgt ervoor dat een user mensen kan onvolgen"

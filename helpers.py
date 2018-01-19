64b5d6905b4cf92a0f528c70fb2bc54cddbd5fa0
Raphael Koning <raphael.koning1@gmail.com>
2018-01-19 17:30:34 +0000

    rate geschreven en dingen aangepast

--- helpers.py
+++ helpers.py
@@ -28,10 +28,37 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def add_user():
+    hash = pwd_context.hash(request.form.get("password"))
+    result = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)", username=request.form.get("username"),hash=hash)
+
+    # ensure that username is not already in use
+    if not result:
+        return apology("username already in use")
+
+    return result
+
 def post():
+    "deze functie zorgt ervoor dat gebruikers foto's kunnen uploaden"
 
 def rate():
-"deze functie zorgt ervoor dat een user foto's kan raten"
+    pictures = db.execute("SELECT * FROM photo")
+    picture_amount = len(pictures)
+    random_int = random.randint(1, picture_amount)
+
+    photo = db.execute("SELECT * FROM photo WHERE photo_id = :photo_id", photo_id = random_int)
+    rated_amount = photo["rated"]
+
+    # dit moet nog in html gezet worden
+    rating = request.form.get("rate")
+
+    old_rating = photo["rating"]
+    new_rating = (old_rating * rated_amount + rating) /(rated_amount + 1)
+    photo_id = photo["photo_id"]
+
+    return db.execute("UPDATE photo SET rating = :rating WHERE photo_id = :photo_id"
+                        , rating = new_rating, photo_id = photo_id)
+
 def follow():
 "deze functie zorgt ervoor dat een user mensen kan volgen"
 def unfollow():
